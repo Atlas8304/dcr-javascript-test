b62c440c0a772e462814cf6121d6fec22f7e6d1e
John Boulding <johnboulding@outlook.com>
2025-08-24 14:02:07 +0100

    Add: table display

--- js/chart.js
+++ js/chart.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('input[type=radio]').click(function(){
         alert(this.value);
-        displayChart(this.value);
+        displayDataCharts(this.value);
     });
 });
 
@@ -56,81 +56,145 @@ function getOptionData(option, data) {
     }
 }
 
-function displayChart(option) {
+function displayBubbleChart(data) {
+    const width = 800
+    const height = 800
+
+    const svg = d3.select("#bubble_chart")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+
+    const min = parsedData.reduce((a, b) => (a.value < b.value ? a : b)).value;
+    const max = parsedData.reduce((a, b) => (a.value > b.value ? a : b)).value;
+
+    console.log(min);
+    console.log(max);
+
+    const size = d3.scaleLinear()
+        .domain([min, max])
+        .range([5,70])
+
+    const Tooltip = d3.select("#bubble_chart")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "tooltip")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "2px")
+        .style("border-radius", "5px")
+        .style("padding", "5px")
+
+    const mouseover = function(event, d) {
+        Tooltip
+            .style("opacity", 1)
+    }
+    const mousemove = function(event, d) {
+        Tooltip
+            .html('<u>' + d.name + '</u>' + "<br>" + d.value)
+            .style("left", (event.x/2+20) + "px")
+            .style("top", (event.y/2-30) + "px")
+    }
+    var mouseleave = function(event, d) {
+        Tooltip
+            .style("opacity", 0)
+    }
+
+    var node = svg.append("g")
+        .selectAll("circle")
+        .data(parsedData)
+        .join("circle")
+            .attr("class", "node")
+            .attr("r", d => size(d.value))
+            .attr("cx", width / 2)
+            .attr("cy", height / 2)
+            .style("fill", "black")
+            .style("fill-opacity", 0.8)
+            .attr("stroke", "black")
+            .style("stroke-width", 1)
+            .on("mouseover", mouseover)
+            .on("mousemove", mousemove)
+            .on("mouseleave", mouseleave)
+
+    const simulation = d3.forceSimulation()
+        .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
+        .force("charge", d3.forceManyBody().strength(.1)) // Nodes are attracted one each other of value is > 0
+        .force("collide", d3.forceCollide().strength(.2).radius(function(d){ return (size(d.value)+3) }).iterations(1)) // Force that avoids circle overlapping
+
+    simulation
+        .nodes(parsedData)
+        .on("tick", function(d){
+            node
+                .attr("cx", d => d.x)
+                .attr("cy", d => d.y)
+        });
+}
+
+function displayTable(data) {
+    var margin = {top: 10, right: 30, bottom: 90, left: 40},
+        width = 2000 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+        
+    const min = parsedData.reduce((a, b) => (a.value < b.value ? a : b)).value;
+    const max = parsedData.reduce((a, b) => (a.value > b.value ? a : b)).value;
+
+
+    var svg = d3.select("#data_table")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    var x = d3.scaleBand()
+        .range([ 0, width ])
+        .domain(data.map(function(d) { return d.name; }))
+        .padding(1);
+
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+            .attr("transform", "translate(-10,0)rotate(-45)")
+            .style("text-anchor", "end");
+    
+    var y = d3.scaleLinear()
+        .domain([min, max])
+        .range([ height, 0]);
+
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    svg.selectAll("myline")
+        .data(data)
+        .enter()
+        .append("line")
+            .attr("x1", function(d) { return x(d.name); })
+            .attr("x2", function(d) { return x(d.name); })
+            .attr("y1", function(d) { return y(d.value); })
+            .attr("y2", y(0))
+            .attr("stroke", "grey")
+
+    svg.selectAll("mycircle")
+        .data(data)
+        .enter()
+        .append("circle")
+            .attr("cx", function(d) { return x(d.name); })
+            .attr("cy", function(d) { return y(d.value); })
+            .attr("r", "4")
+            .style("fill", "#69b3a2")
+            .attr("stroke", "black")
+}
+
+function displayDataCharts(option) {
     $("#bubble_chart").html("");
+    $("#data_table").html("");
     d3.json("/data/countries.json").then(function(data) {
         parsedData = getOptionData(option, data);
-        console.log(parsedData);
-        const width = 800
-        const height = 800
 
-        const svg = d3.select("#bubble_chart")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height)
-
-        const min = parsedData.reduce((a, b) => (a.value < b.value ? a : b)).value;
-        const max = parsedData.reduce((a, b) => (a.value > b.value ? a : b)).value;
-
-        console.log(min);
-        console.log(max);
-
-        const size = d3.scaleLinear()
-            .domain([min, max])
-            .range([5,70])
-
-        const Tooltip = d3.select("#bubble_chart")
-            .append("div")
-            .style("opacity", 0)
-            .attr("class", "tooltip")
-            .style("background-color", "white")
-            .style("border", "solid")
-            .style("border-width", "2px")
-            .style("border-radius", "5px")
-            .style("padding", "5px")
-
-        const mouseover = function(event, d) {
-            Tooltip
-                .style("opacity", 1)
-        }
-        const mousemove = function(event, d) {
-            Tooltip
-                .html('<u>' + d.name + '</u>' + "<br>" + d.value)
-                .style("left", (event.x/2+20) + "px")
-                .style("top", (event.y/2-30) + "px")
-        }
-        var mouseleave = function(event, d) {
-            Tooltip
-                .style("opacity", 0)
-        }
-
-        var node = svg.append("g")
-            .selectAll("circle")
-            .data(parsedData)
-            .join("circle")
-                .attr("class", "node")
-                .attr("r", d => size(d.value))
-                .attr("cx", width / 2)
-                .attr("cy", height / 2)
-                .style("fill", "black")
-                .style("fill-opacity", 0.8)
-                .attr("stroke", "black")
-                .style("stroke-width", 1)
-                .on("mouseover", mouseover)
-                .on("mousemove", mousemove)
-                .on("mouseleave", mouseleave)
-
-        const simulation = d3.forceSimulation()
-            .force("center", d3.forceCenter().x(width / 2).y(height / 2)) // Attraction to the center of the svg area
-            .force("charge", d3.forceManyBody().strength(.1)) // Nodes are attracted one each other of value is > 0
-            .force("collide", d3.forceCollide().strength(.2).radius(function(d){ return (size(d.value)+3) }).iterations(1)) // Force that avoids circle overlapping
-
-        simulation
-            .nodes(parsedData)
-            .on("tick", function(d){
-                node
-                    .attr("cx", d => d.x)
-                    .attr("cy", d => d.y)
-            });
+        displayBubbleChart(parsedData)
+        displayTable(parsedData)
+        
     });
 }
